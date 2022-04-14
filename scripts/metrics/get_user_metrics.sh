31174d8bbfaa79a74e490299c60dacec92ba948c
mireshghallah <fmireshg@eng.ucsd.edu>
2022-04-14 12:29:09 -0700

    Update get_user_metrics.sh

--- scripts/metrics/get_user_metrics.sh
+++ scripts/metrics/get_user_metrics.sh
@@ -7,8 +7,8 @@ python ./codes/metrics/get_user_metrics.py \
 --dir_name_train_out    get_sentence_loss_file_samples_sub_id_long_2.csv_model_pubmed_nsamples_1_date_29_12_2021_15_40_36  \
 --dir_name_population_in  get_sentence_loss_file_i2b2_samples_sub_id_long_2.csv_model_ClinicalBERT_1a_nsamples_1_date_29_12_2021_14_31_21  \
 --dir_name_population_out   get_sentence_loss_file_i2b2_samples_sub_id_long_2.csv_model_pubmed_nsamples_1_date_29_12_2021_16_51_55 \
---file_name_in /home/fmireshg/berglab.projects/sent_extraction/sample_extract/CSV_Files/samples_sub_id_long.csv \
---file_name_ref /home/fmireshg/berglab.projects/sent_extraction/sample_extract/CSV_Files/i2b2_samples_sub_id_long.csv \
---file_name_train /home/fmireshg/berglab.projects/sent_extraction/sample_extract/CSV_Files/samples_sub_id_long_2.csv \
---file_name_population /home/fmireshg/berglab.projects/sent_extraction/sample_extract/CSV_Files/i2b2_samples_sub_id_long_2.csv \
+--file_name_in /home/user/lab.projects/sent_extraction/sample_extract/CSV_Files/samples_sub_id_long.csv \
+--file_name_ref /home/user/lab.projects/sent_extraction/sample_extract/CSV_Files/i2b2_samples_sub_id_long.csv \
+--file_name_train /home/user/lab.projects/sent_extraction/sample_extract/CSV_Files/samples_sub_id_long_2.csv \
+--file_name_population /home/user/lab.projects/sent_extraction/sample_extract/CSV_Files/i2b2_samples_sub_id_long_2.csv \
 
